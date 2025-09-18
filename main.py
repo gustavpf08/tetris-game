53d4b478856eaf7f7415dfe1ebb3ad69e9816a1e
Gustavo Paulino <gugapf08@gmail.com>
2025-09-18 17:17:20 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from config import *
+from sys import exit
+
+class Main:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((LARG_JANELA, COMP_JANELA))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption('Tetris')
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+            # Cor de fundo
+            self.display_surface.fill(CINZA)
+
+            pygame.display.update()
+            self.clock.tick()
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
+
+
+
